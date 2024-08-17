92126df46e48b36d8dde441ca198bba5b5d986dc
Iiemb <iiem.ahmad@ukur.com>
2024-08-17 10:32:04 +0700

    add policies

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "60573056"
+    serviceWorkerVersion: "2030482191"
   }
 });
